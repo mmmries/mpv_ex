39a88f67ff0076a9a8e3b40b6ecb5c7d36fd97ed
Michael Ries <riesmmm@gmail.com>
2023-10-25 13:07:23 +0300

    initial

---
+++ lib/mpv.ex
@@ -0,0 +1,18 @@
+defmodule Mpv do
+  @moduledoc """
+  Documentation for `Mpv`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mpv.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mpv_test.exs
@@ -0,0 +1,8 @@
+defmodule MpvTest do
+  use ExUnit.Case
+  doctest Mpv
+
+  test "greets the world" do
+    assert Mpv.hello() == :world
+  end
+end
